aab9f31eb09d4a89230915d756900a319199388d
Prateek Jain <prateek@Prateeks-MacBook-Pro.local>
2024-05-04 18:53:45 +0800

    modularised the code

--- authentication.py
+++ authentication.py
@@ -7,13 +7,12 @@ def check_authentication(password):
 def authenticate_user():
     if 'authenticated' not in st.session_state:
         st.session_state['authenticated'] = False
-
-    pwd_input = st.sidebar.text_input("Password:", type="password")
-    if st.sidebar.button("Login"):
+    pwd_input = st.text_input("Password:", type="password")
+    if st.button("Login"):
         st.session_state['authenticated'] = check_authentication(pwd_input)
         if not st.session_state['authenticated']:
-            st.sidebar.error("Incorrect password, please try again.")
+            st.error("Incorrect password, please try again.")
         else:
             st.session_state['authenticated'] = True
-            st.sidebar.success("Logged in successfully.")
+            st.success("Logged in successfully.")
             st.rerun()
